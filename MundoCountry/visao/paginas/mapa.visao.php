43969bb51403c9ac9ede1d71db935334376ab5eb
YasminLismare21 <47746511+YasminLismare21@users.noreply.github.com>
2019-04-16 12:37:23 -0300

    Add files via upload

---
+++ MundoCountry/visao/paginas/mapa.visao.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <a href="paginas/sobre">Sobre</a>
+    </body>
+</html>
---
+++ MundoCountry/visao/paginas/sobre.visao.php
@@ -0,0 +1,5 @@
+<html>
+    
+    <p>Essa é a minha loja, onde pode se encontrar roupas country de varias marcas como minuty jeans ,Goyazes entre outros. Desde moda feminina até moda masculina
+        e infantil, com calças jeans, bodys, blusinhas, camisas e outras peças</p>
+</html>
